0c5ef67c86166d1fc26937950bec426ff3a93d57
Victor <victorcost_@outlook.com>
2022-05-16 10:14:48 -0300

    stable version, new feat checking IDS

--- default_models/vtex_spreadsheet.py
+++ default_models/vtex_spreadsheet.py
@@ -9,14 +9,21 @@ class VtexSpreadsheet(metaclass=abc.ABCMeta):
 
     @abc.abstractmethod
     def colect_data(self):
+        """Essa função recebe um DataFrame e percorre as suas linhas, iniciando o mapeamento da planilha de
+        especificações, chamando o método responsável por preencher o dicionário para estruturar a planilha de
+        especificações."""
         ...
 
     @abc.abstractmethod
     def fill_lists(self, *args):
+        """Cria um dicionário com SKU como chave, com os campos necessários para o preenchimento da planilha de
+               imagens """
         ...
 
     @abc.abstractmethod
     def build_vtex_spreadsheet(self):
+        """Transforma as informações presentes no dicionário de imagens em listas para transformá-las em linhas e alimentar
+                o DataFrame."""
         ...
 
     def init_process(self) -> list[str, build_vtex_spreadsheet]:
